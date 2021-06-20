1858217f2ac1aa2527cd5900bf8d9e714e32a01e
Joan Andrés <andres6936@live.com>
2021-06-19 20:50:36 -0500

    Added documentation

--- Include/Protocol/conf.h
+++ Include/Protocol/conf.h
@@ -44,10 +44,31 @@ struct _Tn5250Config : private std::vector<std::pair<std::string, std::string>>
 
 	_Tn5250Config() noexcept = default;
 
+	/**
+	 * Associates the specified value with the specified key in this map
+	 * (optional operation).
+	 *
+	 * @param key key with which the specified value is to be associated.
+	 * @param value value to be associated with the specified key.
+	 */
 	void put(const std::string& key, const std::string& value) noexcept;
 
+	/**
+	 * Returns true if this map contains a mapping for the specified key.
+	 *
+	 * @param key key whose presence in this map is to be tested.
+	 * @return true if this map contains a mapping for the specified key.
+	 */
 	const bool containsKey(const std::string& key) const noexcept;
 
+	/**
+	 * @brief Returns the value to which the specified key is mapped,
+	 * or null if this map contains no mapping for the key.
+	 *
+	 * @param key the key whose associated value is to be returned.
+	 * @return the value to which the specified key is mapped, or null
+	 * if this map contains no mapping for the key.
+	 */
 	const std::string get(const std::string& key) const noexcept;
 };
 
